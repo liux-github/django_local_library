8c8b213652c8c6021376b8bb5e637c18a6829c17
liux <liu-xin@zhimingsoft.cn>
2022-02-25 09:47:34 +0800

    runtime.txt edit

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-b2q4_^lb=&fop9
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shrouded-tor-87420.herokuapp.com',]
 
 
 # Application definition
